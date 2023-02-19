5afa9cd87a844f33fa4047be836ecb12f4d4a6d5
Ana Kloss <anak492@gmail.com>
2023-02-19 11:07:39 -0300

    (Student/update): views and urls

--- estudiante/urls.py
+++ estudiante/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from estudiante import views
 
 urlpatterns = [
-    path("students/", views.student, name="Estudiantes"),
-    path("students/addStudent/", views.addStudent),
-    path("students/editStudent/<int:id_student>", views.editStudent),
+    path("", views.student, name="Estudiantes"),
+    path("addStudent/", views.addStudent),
+    path("editStudent/<int:id_student>", views.editStudent),
     path("updateStudent/", views.updateStudent),
-    path("students/deleteStudent/<int:id_student>", views.deleteStudent),
+    path("deleteStudent/<int:id_student>", views.deleteStudent),
 ]
